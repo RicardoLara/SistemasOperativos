cfe9cbdbaf316c820e24888ae73d8c342e446f77
RicardoLara <richard-o95@hotmail.com>
2016-03-12 19:06:31 -0600

    Errors

--- Practicas/practica3_NotFalse.c
+++ Practicas/practica3_NotFalse.c
@@ -6,21 +6,26 @@
 char archivos[5][50];
 
 void * buscaCosas(void * p){
+	printf("Si entre sl hilo\n");
 	int * cual = (int *)p;
 	//Buscar cosas en archivos[*cual]
+	printf("Soy el hilo con %d <- este archivo",*cual);
 }
 
 int main(int argc, char ** argv){
 	int i;
-	pthread_t hilo;
-	if(argc != 5)
+	pthread_t hilos[5];
+	printf("Estoy cachando %d parametros\n",argc);
+
+	if(argc != 6)
 		printf("Uy, te mamaste, mala entrada\n");
 	else{
 		for(i=0; i<5; i++){
-			strcat(archivos[i],atoi(argv[i+1]));
-			printf("Archivo %d: %s\n",i,archivos[i]);
-			if(!pthread_create(hilo,NULL,buscaCosas,i)) printf(":D\n");
-			else printf("Error creando hilo\n");
+			strcat(archivos[i],argv[i+1]);
+			printf("Archivo %d: %s.\n",i,archivos[i]);
+			if(pthread_create(&hilos[i],NULL,buscaCosas,(void *)i))
+				printf("Error creando hilo\n");
+			else printf("Cree el hilo de archivo %d. \n",i);
 		}
 		/*
 			Para cada archivo, crear un hilo P
